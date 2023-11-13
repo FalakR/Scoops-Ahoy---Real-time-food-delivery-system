90de0cc1b6aeb9541b9e4954cf306da6fc7a7b82
nehmat-kaur <117527882+nehmat-kaur@users.noreply.github.com>
2023-11-13 12:14:24 -0500

    CHANGES TO LOGIN: Files in use_cases and interface_adapters packages updated appropriately.

--- src/interface_adapters/login/LoginController.java
+++ src/interface_adapters/login/LoginController.java
@@ -1,4 +1,22 @@
 package interface_adapters.login;
 
+import use_cases.log_in.LoginInputData;
+import use_cases.log_in.LoginInputBoundary;
+import use_cases.sign_up.SignupInputBoundary;
+import use_cases.sign_up.SignupInputData;
+
 public class LoginController {
+
+    final LoginInputBoundary loginUseCaseInteractor;
+    public LoginController(LoginInputBoundary loginUseCaseInteractor) {
+        this.loginUseCaseInteractor = loginUseCaseInteractor;
+    }
+
+
+    public void execute(String email, String password) {
+        LoginInputData loginInputData = new LoginInputData(
+                email, password);
+
+        loginUseCaseInteractor.execute(loginInputData);
+    }
 }
--- src/use_cases/log_in/LoginInteractor.java
+++ src/use_cases/log_in/LoginInteractor.java
@@ -1,4 +1,34 @@
 package use_cases.log_in;
 
-public class LoginInteractor {
+import entities.User;
+
+public class LoginInteractor implements LoginInputBoundary {
+    final LoginDataAccessInterface userDataAccessObject;
+    final LoginOutputBoundary loginPresenter;
+
+    public LoginInteractor(LoginDataAccessInterface userDataAccessInterface,
+                           LoginOutputBoundary loginOutputBoundary) {
+        this.userDataAccessObject = userDataAccessInterface;
+        this.loginPresenter = loginOutputBoundary;
+    }
+
+    @Override
+    public void execute(LoginInputData loginInputData) {
+        String username = loginInputData.getEmail();
+        String password = loginInputData.getPassword();
+        if (!userDataAccessObject.existsByEmail(username)) {
+            loginPresenter.prepareFailView(username + ": Account does not exist.");
+        } else {
+            String pwd = userDataAccessObject.get(username).getPassword();
+            if (!password.equals(pwd)) {
+                loginPresenter.prepareFailView("Incorrect password for " + username + ".");
+            } else {
+
+                User user = userDataAccessObject.get(loginInputData.getEmail());
+
+                LoginOutputData loginOutputData = new LoginOutputData(user.getEmail(), false);
+                loginPresenter.prepareSuccessView(loginOutputData);
+            }
+        }
+    }
 }
