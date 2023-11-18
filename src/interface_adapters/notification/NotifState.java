af92be14f5741366ab89de51bd5dcf8f05768b84
Gurveen Sahni <gurveenkaursahni@gmail.com>
2023-11-18 13:37:17 -0500

    changes added to notification use case

--- src/interface_adapters/notification/NotifState.java
+++ src/interface_adapters/notification/NotifState.java
@@ -1,4 +1,19 @@
 package interface_adapters.notification;
 
 public class NotifState {
+    private String couriername = "";
+
+    public NotifState(NotifState copy) {
+        couriername = copy.couriername;
+    }
+
+    // Because of the previous copy constructor, the default constructor must be explicit.
+    public NotifState() {}
+
+    public String getCourierName() {
+        return couriername;
+    }
+    public void setCourierName(String couriername) {
+        this.couriername = couriername;
+    }
 }
