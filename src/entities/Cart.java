77b356c1cbed7409d049d509704b0ac4666bbc14
falakrastogi <falakrast1@gmail.com>
2023-11-18 16:37:11 -0500

    Cart commits

--- src/entities/Cart.java
+++ src/entities/Cart.java
@@ -10,5 +10,41 @@ public class Cart {
         items.add(iceCream);
     }
 
-    // Other cart-related methods
+    public void removeItem(IceCream iceCream) {
+        items.remove(iceCream);
+    }
+
+    public void clearCart() {
+        items.clear();
+    }
+
+    public List<IceCream> getItems() {
+        return new ArrayList<>(items);
+    }
+
+    public double calculateTotalPrice() {
+        return items.stream().mapToDouble(IceCream::getPrice).sum();
+    }
+
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder cartContents = new StringBuilder("Cart Contents:\n");
+
+        if (isEmpty()) {
+            cartContents.append("Empty\n");
+        } else {
+            for (IceCream iceCream : items) {
+                cartContents.append(String.format("- %s (%s) - $%.2f\n",
+                        iceCream.getName(), iceCream.getFlavour(), iceCream.getPrice()));
+            }
+            cartContents.append(String.format("Total: $%.2f\n", calculateTotalPrice()));
+        }
+
+        return cartContents.toString();
+    }
 }
+
--- src/use_cases/add_to_cart/AddToCartDataAccessInterface.java
+++ src/use_cases/add_to_cart/AddToCartDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_cases.add_to_cart;
 
+import entities.Cart;
+import entities.IceCream;
+
 public interface AddToCartDataAccessInterface {
+    void addToCart(IceCream iceCream);
+    Cart getCart();
 }
