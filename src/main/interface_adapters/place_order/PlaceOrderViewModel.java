eae673f3e0d1846126a0c73d1697baf01531caea
Gurveen Sahni <gurveenkaursahni@gmail.com>
2023-11-26 16:56:37 -0500

    changes made to PlaceOrder

--- src/main/interface_adapters/place_order/PlaceOrderViewModel.java
+++ src/main/interface_adapters/place_order/PlaceOrderViewModel.java
@@ -1,5 +1,44 @@
 package interface_adapters.place_order;
 
-public class PlaceOrderViewModel {
+import interface_adapters.ViewModel;
+import interface_adapters.track.TrackState;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class PlaceOrderViewModel extends ViewModel {
+
+    public final String TITLE_LABEL = "Place Order View";
+    public final String ADDRESS_LABEL = "Enter delivery address";
+    public final String CARD_NUMBER_LABEL = "Enter credit card number";
+    public final String CVV_LABEL = "Enter CVV";
+    public final String EXPIRY_DATE_LABEL = "Enter expiry date";
+
+    public final String PLACE_ORDER_BUTTON_LABEL = "Place Order";
+
+    private PlaceOrderState state = new PlaceOrderState();
+
+    public PlaceOrderViewModel() {
+        super("place_order");
+    }
+
+    public void setState(PlaceOrderState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // This is what the PlaceOrder Presenter will call to let the ViewModel know
+    // to alert the View
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public PlaceOrderState getState() {
+        return state;
+    }
 }
-// TODO: Create getViewName method.
