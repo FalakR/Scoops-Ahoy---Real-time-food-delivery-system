47bb9d3a89aa927f9d09f5ba96a75c2e5ec821ea
Gurveen Sahni <gurveenkaursahni@gmail.com>
2023-11-27 12:27:28 -0500

    Made changes to PlaceOrderController

--- src/main/interface_adapters/place_order/PlaceOrderController.java
+++ src/main/interface_adapters/place_order/PlaceOrderController.java
@@ -1,4 +1,23 @@
 package interface_adapters.place_order;
 
+import entities.CommonCart;
+import entities.IceCream;
+import use_cases.place_order.PlaceOrderInputBoundary;
+import use_cases.place_order.PlaceOrderInputData;
+
+import java.util.List;
+
 public class PlaceOrderController {
+    final PlaceOrderInputBoundary placeOrderUseCaseInteractor;
+
+    public PlaceOrderController(PlaceOrderInputBoundary placeOrderUseCaseInteractor) {
+        this.placeOrderUseCaseInteractor = placeOrderUseCaseInteractor;
+    }
+
+    public void execute(CommonCart cart, List<IceCream> iceCreams, String userAddress, String creditCardNumber, int cvv, String expiryDate){
+        PlaceOrderInputData placeOrderInputData= new PlaceOrderInputData(cart, iceCreams, userAddress, creditCardNumber, cvv, expiryDate);
+
+        placeOrderUseCaseInteractor.execute(placeOrderInputData);
+    }
+
 }
