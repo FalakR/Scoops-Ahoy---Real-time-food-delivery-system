125287b68475e40135155c91aca8ae02bd430d2b
falakrastogi <falakrast1@gmail.com>
2023-12-02 15:38:07 -0500

    Created 2 more tests for AddToCart related entities.

---
+++ src/test/entities/CommonIceCreamTest.java
@@ -0,0 +1,15 @@
+package entities;
+
+import junit.framework.TestCase;
+
+public class CommonIceCreamTest extends TestCase {
+
+    public void testTestGetName() {
+    }
+
+    public void testGetFlavour() {
+    }
+
+    public void testGetPrice() {
+    }
+}
