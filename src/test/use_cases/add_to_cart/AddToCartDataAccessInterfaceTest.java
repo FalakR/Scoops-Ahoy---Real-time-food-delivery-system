c2f891dd1744a686c4214620db267d962b1917f9
falakrastogi <falakrast1@gmail.com>
2023-12-02 15:34:24 -0500

    Created 3 more tests for AddToCart use case and interface adapter

---
+++ src/test/use_cases/add_to_cart/AddToCartDataAccessInterfaceTest.java
@@ -0,0 +1,9 @@
+package use_cases.add_to_cart;
+
+import junit.framework.TestCase;
+
+public class AddToCartDataAccessInterfaceTest extends TestCase {
+
+    public void testGetIceCream() {
+    }
+}
