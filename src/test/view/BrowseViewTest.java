ae55a3bec6469fce3f17a9000d6dc5a759c88ee0
falakrastogi <falakrast1@gmail.com>
2023-12-03 14:27:52 -0500

    Created BrowseViewTest and cleaned AddToCartUseCaseFactory

---
+++ src/test/view/BrowseViewTest.java
@@ -0,0 +1,132 @@
+package view;
+
+import data_access.FileIceCreamDataAccessObject;
+import entities.Cart;
+import entities.CommonIceCreamFactory;
+import entities.IceCream;
+import entities.IceCreamFactory;
+import interface_adapters.ViewManagerModel;
+import interface_adapters.add_to_cart.AddToCartController;
+import interface_adapters.add_to_cart.AddToCartPresenter;
+import interface_adapters.add_to_cart.AddToCartViewModel;
+import interface_adapters.place_order.PlaceOrderViewModel;
+import org.junit.Before;
+import org.junit.Test;
+import use_cases.add_to_cart.AddToCartInputBoundary;
+import use_cases.add_to_cart.AddToCartInputData;
+import use_cases.add_to_cart.AddToCartOutputBoundary;
+import use_cases.add_to_cart.AddToCartOutputData;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class BrowseViewTest {
+
+
+    private AddToCartViewModel addToCartViewModel1;
+    private ViewManagerModel viewManagerModel;
+    private PlaceOrderViewModel placeOrderViewModel;
+    private TestAddToCartController addToCartController;
+    private TestAddToCartPresenter addToCartPresenter;
+    private TestAddToCartViewModel addToCartViewModel;
+    private TestFileIceCreamDataAccessObject fileIceCreamDataAccessObject;
+    private BrowseView browseView;
+    private AddToCartInputBoundary addToCartInputBoundary;
+
+    @Before
+    public void setUp() throws IOException {
+
+        IceCreamFactory iceCreamFactory1 = new CommonIceCreamFactory();
+
+
+        addToCartController = new TestAddToCartController(addToCartInputBoundary);
+        addToCartPresenter = new TestAddToCartPresenter(viewManagerModel, addToCartViewModel1, placeOrderViewModel);
+        addToCartViewModel = new TestAddToCartViewModel();
+        fileIceCreamDataAccessObject = new TestFileIceCreamDataAccessObject(iceCreamFactory1);
+
+        browseView = new BrowseView(addToCartController, addToCartPresenter, addToCartViewModel, fileIceCreamDataAccessObject);
+    }
+
+    @Test
+    public void testActionPerformed() {
+        // Example: Test the actionPerformed method
+        browseView.actionPerformed(null); // Pass a dummy ActionEvent since it's not used in your method
+
+        // Add assertions based on the expected behavior
+    }
+
+    @Test
+    public void testPropertyChange() {
+        // Example: Test the propertyChange method
+        browseView.propertyChange(null); // Pass a dummy PropertyChangeEvent since it's not used in your method
+
+        // Add assertions based on the expected behavior
+    }
+
+    // Mock implementation of AddToCartController for testing
+    static class TestAddToCartController extends AddToCartController {
+
+        private boolean executeCalled = false;
+
+        public TestAddToCartController(AddToCartInputBoundary addToCartUseCaseInteractor) {
+            super(addToCartUseCaseInteractor);
+        }
+
+        @Override
+        public void execute(ArrayList<IceCream> iceCreams) {
+            executeCalled = true;
+        }
+
+        public boolean isExecuteCalled() {
+            return executeCalled;
+        }
+    }
+
+    // Mock implementation of AddToCartPresenter for testing
+    static class TestAddToCartPresenter extends AddToCartPresenter {
+
+        private boolean prepareSuccessViewCalled = false;
+
+        public TestAddToCartPresenter(ViewManagerModel viewManagerModel, AddToCartViewModel addToCartViewModel, PlaceOrderViewModel placeOrderViewModel) {
+            super(viewManagerModel, addToCartViewModel, placeOrderViewModel);
+        }
+
+        @Override
+        public void prepareSuccessView(AddToCartOutputData cart) {
+            prepareSuccessViewCalled = true;
+        }
+
+        public boolean isPrepareSuccessViewCalled() {
+            return prepareSuccessViewCalled;
+        }
+    }
+
+    // Mock implementation of AddToCartViewModel for testing
+    static class TestAddToCartViewModel extends AddToCartViewModel {
+
+        // Implement methods if needed for testing
+    }
+
+    // Mock implementation of FileIceCreamDataAccessObject for testing
+
+    static class TestFileIceCreamDataAccessObject extends FileIceCreamDataAccessObject {
+
+        private static final String TEST_CSV_PATH = "./icecreamsinfo.csv"; // Replace with your actual test CSV path
+
+        public TestFileIceCreamDataAccessObject(IceCreamFactory iceCreamFactory) throws IOException {
+            super(TEST_CSV_PATH, iceCreamFactory);
+        }
+
+        // Additional methods if needed for testing
+
+        // Override methods if needed for testing
+
+        // For example, you might want to override getIceCream to provide custom behavior for testing
+        @Override
+        public IceCream getIceCream(String name) {
+            // Implement custom behavior for testing
+            return super.getIceCream(name);
+        }
+    }
+
+}
