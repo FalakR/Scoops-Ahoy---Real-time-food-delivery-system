e2eedd45ed2fb44611dcdf412519ceb6317eb3d9
Gurveen Sahni <gurveenkaursahni@gmail.com>
2023-12-03 15:46:18 -0500

    Test added for PlaceOrderInteractor

--- src/test/use_cases/place_order/PlaceOrderInteractorTest.java
+++ src/test/use_cases/place_order/PlaceOrderInteractorTest.java
@@ -1,25 +1,45 @@
-//package use_cases.place_order;
-//
-//
-//import data_access.AblyDataAccessObject;
-//import data_access.InMemoryDataAccessObject;
-//import data_access.InMemoryUserDataAccessObject;
-//import entities.CommonCart;
-//import entities.IceCream;
-//import io.ably.lib.types.AblyException;
+package use_cases.place_order;
+
+
+import data_access.AblyDataAccessObject;
+import data_access.InMemoryDataAccessObject;
+import entities.CommonCart;
+import entities.CommonIceCreamFactory;
+import entities.IceCream;
+import entities.IceCreamFactory;
+import io.ably.lib.types.AblyException;
 //import org.junit.jupiter.api.Test;
-//import use_cases.track_order.TrackOrderInputBoundary;
-//import use_cases.track_order.TrackOrderInteractor;
+import junit.framework.TestCase;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import use_cases.track_order.*;
+
+import java.util.ArrayList;
+
+public class PlaceOrderInteractorTest extends TestCase{
+//    public void testPlaceOrderSummaryView() throws AblyException {
+//        IceCreamFactory factory = new CommonIceCreamFactory();
 //
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//        IceCream iceCream1 = factory.create("ChocolateChip","Cookie Dough",10);
+//        System.out.println(iceCream1 + "created");
+//        ArrayList<IceCream> list = new ArrayList<>();
+//        list.add(iceCream1);
 //
-//class PlaceOrderInteractorTest {
-//    @Test
-//    void PlaceOrderSummaryView() throws AblyException {
-//        PlaceOrderInputData inputData= new PlaceOrderInputData(new CommonCart(null), "19 tower Road", "111",Integer.toString(123),"06062026");
+//        CommonCart cart = new CommonCart(list);
+//        String userAddress = "123 Main St";
+//        String creditCardNumber = "1234567890123456";
+//        String cvv = "123";
+//        String expiryDate = "1223";
+//
+//        PlaceOrderInputData inputData= new PlaceOrderInputData(cart, userAddress, creditCardNumber,cvv,expiryDate);
 //        PlaceOrderDataAccessInterface ablyDataAccessObject = new AblyDataAccessObject();
 //        PlaceOrderUserDataAccessInterface userRepository = new InMemoryDataAccessObject();
+//        TrackOrderDataAccessInterface mocktrackOrderDataAccessObject =  new AblyDataAccessObject();
+//        TrackOrderUserDataAccessInterface mocktrackOrderUserDataAccessObject = new InMemoryDataAccessObject();
+//        TrackOrderOutputBoundary mocktrackOrderPresenter= new MockTrackOrderPresenter();
+//        TrackOrderInteractor mockTrackOrderInteractor = new TrackOrderInteractor(mocktrackOrderDataAccessObject,
+//                mocktrackOrderUserDataAccessObject,mocktrackOrderPresenter);
 //
 //
 //        PlaceOrderOutputBoundary myPresenter = new PlaceOrderOutputBoundary() {
@@ -30,17 +50,110 @@
 //            }
 //
 //
-//            public void prepareChangeView() {
-//                fail("Screen has already changed");
-//            }
-//
 //            @Override
 //            public void prepareFailView(String s) {
 //                fail("Use case failure is unexpected");
 //            }
 //        };
-//        //PlaceOrderInputBoundary interactor = new PlaceOrderInteractor(myPresenter, ablyDataAccessObject, userRepository,new TrackOrderInteractor());
-//        //interactor.execute(inputData);
-//    }
+//        PlaceOrderInputBoundary interactor = new PlaceOrderInteractor(myPresenter, ablyDataAccessObject, userRepository,mockTrackOrderInteractor);
+//        interactor.execute(inputData);
 //
-//}
+//    }
+
+    private static class MockTrackOrderPresenter implements TrackOrderOutputBoundary {
+
+
+        @Override
+        public void prepareView(TrackOrderOutputData data) {
+
+        }
+    }
+    public void testTestFailureUserAddressEmpty() throws AblyException {
+        IceCreamFactory factory = new IceCreamFactory() {
+            @Override
+            public IceCream create(String name, String flavour, Integer price) {
+                return null;
+            }
+        };
+
+        IceCream iceCream1 = factory.create("ChocolateChip","Cookie Dough",10);
+
+        ArrayList<IceCream> list = new ArrayList<>();
+        list.add(iceCream1);
+
+        CommonCart cart = new CommonCart(list);
+        String userAddress = "";
+        String creditCardNumber = "1234567890123456";
+        String cvv = "123";
+        String expiryDate = "1223";
+
+        PlaceOrderInputData inputData= new PlaceOrderInputData(cart, userAddress, creditCardNumber,cvv,expiryDate);
+        PlaceOrderDataAccessInterface ablyDataAccessObject = new AblyDataAccessObject();
+        PlaceOrderUserDataAccessInterface userRepository = new InMemoryDataAccessObject();
+        TrackOrderDataAccessInterface mocktrackOrderDataAccessObject =  new AblyDataAccessObject();
+        TrackOrderUserDataAccessInterface mocktrackOrderUserDataAccessObject = new InMemoryDataAccessObject();
+        TrackOrderOutputBoundary mocktrackOrderPresenter= new MockTrackOrderPresenter();
+        TrackOrderInteractor mockTrackOrderInteractor = new TrackOrderInteractor(mocktrackOrderDataAccessObject,
+                mocktrackOrderUserDataAccessObject,mocktrackOrderPresenter);
+        PlaceOrderOutputBoundary myPresenter = new PlaceOrderOutputBoundary() {
+            @Override
+            public void prepareSummaryView(PlaceOrderOutputData order) {
+                fail("Use case success is unexpected");
+            }
+
+
+            @Override
+            public void prepareFailView(String s) {
+                assertEquals("User address cannot be empty.", s);
+            }
+        };
+        PlaceOrderInputBoundary interactor = new PlaceOrderInteractor(myPresenter, ablyDataAccessObject, userRepository,mockTrackOrderInteractor);
+        interactor.execute(inputData);
+
+    }
+
+    public void testTestFailureCreditcard() throws AblyException {
+        IceCreamFactory factory = new IceCreamFactory() {
+            @Override
+            public IceCream create(String name, String flavour, Integer price) {
+                return null;
+            }
+        };
+
+        IceCream iceCream1 = factory.create("ChocolateChip","Cookie Dough",10);
+
+        ArrayList<IceCream> list = new ArrayList<>();
+        list.add(iceCream1);
+
+        CommonCart cart = new CommonCart(list);
+        String userAddress = "19 Tower Road";
+        String creditCardNumber = "";
+        String cvv = "123";
+        String expiryDate = "1223";
+
+        PlaceOrderInputData inputData= new PlaceOrderInputData(cart, userAddress, creditCardNumber,cvv,expiryDate);
+        PlaceOrderDataAccessInterface ablyDataAccessObject = new AblyDataAccessObject();
+        PlaceOrderUserDataAccessInterface userRepository = new InMemoryDataAccessObject();
+        TrackOrderDataAccessInterface mocktrackOrderDataAccessObject =  new AblyDataAccessObject();
+        TrackOrderUserDataAccessInterface mocktrackOrderUserDataAccessObject = new InMemoryDataAccessObject();
+        TrackOrderOutputBoundary mocktrackOrderPresenter= new MockTrackOrderPresenter();
+        TrackOrderInteractor mockTrackOrderInteractor = new TrackOrderInteractor(mocktrackOrderDataAccessObject,
+                mocktrackOrderUserDataAccessObject,mocktrackOrderPresenter);
+        PlaceOrderOutputBoundary myPresenter = new PlaceOrderOutputBoundary() {
+            @Override
+            public void prepareSummaryView(PlaceOrderOutputData order) {
+                fail("Use case success is unexpected");
+            }
+
+
+            @Override
+            public void prepareFailView(String s) {
+                assertEquals("Invalid credit card information.", s);
+            }
+        };
+        PlaceOrderInputBoundary interactor = new PlaceOrderInteractor(myPresenter, ablyDataAccessObject, userRepository,mockTrackOrderInteractor);
+        interactor.execute(inputData);
+
+    }
+
+}
