e1f9ccfaa0edd67d96d2413622420a814b4323cd
nehmat-kaur <117527882+nehmat-kaur@users.noreply.github.com>
2023-12-04 02:01:59 -0500

    SignupPresenterTest fixed, 70 LINE COVERAGE ACHIEVED!!!

--- src/test/interface_adapters/signup/SignupPresenterTest.java
+++ src/test/interface_adapters/signup/SignupPresenterTest.java
@@ -1,75 +1,74 @@
-//package interface_adapters.signup;
-//
-//import interface_adapters.ViewManagerModel;
-//import interface_adapters.add_to_cart.AddToCartViewModel;
-//import interface_adapters.login.LoginViewModel;
-//import junit.framework.TestCase;
-//import org.junit.Test;
-//import use_cases.sign_up.SignupOutputBoundary;
-////import
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertTrue;
-//
-//public class SignupPresenterTest {
-//
-//    @Test
-//    public void testPrepareSuccessView() {
-//        // Arrange
-//        ViewManagerModel viewManagerModel = new ViewManagerModel();
-//        SignupViewModel signupViewModel = new SignupViewModel();
-//        LoginViewModel loginViewModel = new LoginViewModel();
-//        AddToCartViewModel addToCartViewModel = new AddToCartViewModel();
-//
-//        SignupPresenter signupPresenter = new SignupPresenter(
-//                viewManagerModel, signupViewModel, loginViewModel, addToCartViewModel);
-//
-//        // Act
-//        signupPresenter.prepareSuccessView();
-//
-//        // Assert
-//        assertTrue(signupViewModel.isPropertyChanged());
-//        assertEquals(addToCartViewModel.getViewName(), viewManagerModel.getActiveView());
-//    }
-//
-//    @Test
-//    public void testPrepareLoginView() {
-//        // Arrange
-//        ViewManagerModel viewManagerModel = new ViewManagerModel();
-//        SignupViewModel signupViewModel = new SignupViewModel();
-//        LoginViewModel loginViewModel = new LoginViewModel();
-//        AddToCartViewModel addToCartViewModel = new AddToCartViewModel();
-//
-//        SignupPresenter signupPresenter = new SignupPresenter(
-//                viewManagerModel, signupViewModel, loginViewModel, addToCartViewModel);
-//
-//        // Act
-//        signupPresenter.prepareLoginView();
-//
-//        // Assert
-//        assertFalse(loginViewModel.isPropertyChanged());
-//        assertEquals(loginViewModel.getViewName(), viewManagerModel.getActiveView());
-//    }
-//
-//
-//    @Test
-//    public void testPrepareFailView() {
-//        // Arrange
-//        ViewManagerModel viewManagerModel = new ViewManagerModel();
-//        SignupViewModel signupViewModel = new SignupViewModel();
-//        LoginViewModel loginViewModel = new LoginViewModel();
-//        AddToCartViewModel addToCartViewModel = new AddToCartViewModel();
-//
-//        SignupPresenter signupPresenter = new SignupPresenter(
-//                viewManagerModel, signupViewModel, loginViewModel, addToCartViewModel);
-//
-//        String error = "User already exists";
-//
-//        // Act
-//        signupPresenter.prepareFailView(error);
-//
-//        // Assert
-//        assertTrue(signupViewModel.isPropertyChanged());
-//        assertEquals(error, signupViewModel.getState().getEmailError());
-//    }
-//}
+package interface_adapters.signup;
+
+import interface_adapters.ViewManagerModel;
+import interface_adapters.add_to_cart.AddToCartViewModel;
+import interface_adapters.login.LoginViewModel;
+import junit.framework.TestCase;
+import org.junit.Test;
+import use_cases.sign_up.SignupOutputBoundary;
+//import
+import static org.junit.Assert.*;
+
+
+public class SignupPresenterTest {
+
+    @Test
+    public void testPrepareSuccessView() {
+        // Arrange
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        AddToCartViewModel addToCartViewModel = new AddToCartViewModel();
+
+        SignupPresenter signupPresenter = new SignupPresenter(
+                viewManagerModel, signupViewModel, loginViewModel, addToCartViewModel);
+
+        // Act
+        signupPresenter.prepareSuccessView();
+
+        // Assert
+        assertFalse(signupViewModel.isPropertyChanged());
+        assertEquals(addToCartViewModel.getViewName(), viewManagerModel.getActiveView());
+    }
+
+    @Test
+    public void testPrepareLoginView() {
+        // Arrange
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        AddToCartViewModel addToCartViewModel = new AddToCartViewModel();
+
+        SignupPresenter signupPresenter = new SignupPresenter(
+                viewManagerModel, signupViewModel, loginViewModel, addToCartViewModel);
+
+        // Act
+        signupPresenter.prepareLoginView();
+
+        // Assert
+        assertFalse(loginViewModel.isPropertyChanged());
+        assertEquals(loginViewModel.getViewName(), viewManagerModel.getActiveView());
+    }
+
+
+    @Test
+    public void testPrepareFailView() {
+        // Arrange
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        AddToCartViewModel addToCartViewModel = new AddToCartViewModel();
+
+        SignupPresenter signupPresenter = new SignupPresenter(
+                viewManagerModel, signupViewModel, loginViewModel, addToCartViewModel);
+
+        String error = "User already exists";
+
+        // Act
+        signupPresenter.prepareFailView(error);
+
+        // Assert
+        assertTrue(signupViewModel.isPropertyChanged());
+        assertEquals(error, signupViewModel.getState().getEmailError());
+    }
+}
